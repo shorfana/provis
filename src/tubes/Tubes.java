02dfc9b2b828e55089f44bd4d7b6279417a1c093
iqbal <iqbalshorfana@gmail.com>
2018-07-08 18:25:46 +0700

    Start Project
    frame utama dan frame mahasiswa udah

---
+++ src/tubes/Tubes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+import tubes.frame.frame_utama_if;
+
+/**
+ *
+ * @author Shorfana
+ */
+public class Tubes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frame_utama_if frm = new frame_utama_if();
+        frm.setVisible(true);
+    }
+    
+}
